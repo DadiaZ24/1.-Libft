c535e80d571be1283ddbba710c2ff9c1e8654ddc
Diogo Ferreira <155843454+DadiaZ24@users.noreply.github.com>
2024-06-29 19:43:53 +0100

    Add files via upload

--- tester/main.c
+++ tester/main.c
@@ -1,30 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tester.h                                           :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddias-fe <ddias-fe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/29 19:06:20 by ddias-fe          #+#    #+#             */
-/*   Updated: 2024/06/29 19:06:20 by ddias-fe         ###   ########.fr       */
+/*   Created: 2024/06/29 19:04:32 by ddias-fe          #+#    #+#             */
+/*   Updated: 2024/06/29 19:04:32 by ddias-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TESTER_H
-# define TESTER_H
+#include "tester.h"
 
-#include "../libft/libft.h"
-
-void    ft_atoi_test(void);
-void	run_tester(int user_result, int expected_result, char *test)
+int main(void)
 {
-	if (user_result != expected_result)
-	{
-		fprintf(stderr, "Test failed!! ----> [%s]\n", test);
-		fprintf(stderr, "Expected result = [%d]\n", expected_result);
-		fprintf(stderr, "Your result = [%d]\n", user_result);
-		exit(0);
-	}
+    //ATOI TESTER
+    ft_atoi_test();
+    return (0);
 }
-
-#endif
